7fafc4602f310b3faaf70bcb5e0adce037d7e422
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-09-21 13:44:07 -0400

    make the marker red and center at blair

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {Map, Marker} from 'pigeon-maps'
+import { stamenToner } from 'pigeon-maps/providers'
 
 export default function Home() {
   return (
@@ -7,8 +8,8 @@ export default function Home() {
       <h1 className="text-4xl font-bold mt-5">QRoute</h1>
 
       <div className="mt-5 mx-20 rounded-xl">
-        <Map height={500} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-          <Marker width={50} anchor={[50.879, 4.6997]} />
+        <Map height={500} defaultCenter={[39.018, -77.012]} defaultZoom={11}>
+          <Marker width={40} anchor={[39.018, -77.012]} color = "#c92e39"/>
         </Map>
       </div>
     </div>
