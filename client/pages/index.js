76b844ac4b696170ba3bdbe5773bac999a320623
tinuvanapamula@gmail.com <tinuvanapamula@gmail.com>
2022-10-17 23:02:01 -0400

    fix build issue

--- client/pages/index.js
+++ client/pages/index.js
@@ -335,6 +335,7 @@ export default function Home() {
 					<div
 						id={`tooltip-hover-${i}`}
 						role="tooltip"
+						key={i}
 						className="inline-block absolute invisible z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
 					>
 						{address}
